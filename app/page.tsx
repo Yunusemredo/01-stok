bca9e55d43909c23847ac3a109ff657ffc710ef1
Yunus Emre DONMEZ <emre2002-42@hotmail.com>
2026-05-01 18:53:08 +0300

    basardım-selin

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Next.js bizim sunucu sikiş</h1>
+      <h1>Başardım aşkııııım başardııııım</h1>
     </>
   );
 }
